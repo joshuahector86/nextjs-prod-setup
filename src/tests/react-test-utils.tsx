c27eff2a37c97a9e44e64bb88300b5f070b79d64
Dimitri Hector <joshuahector86@gmail.com>
2024-12-12 13:37:28 -0500

    chore(adding postress endpoints and testing to environment): this just adds postrgress env
    This change adds the set up for a postrgress enviornment to be done with prisma. Please ensure that
    you update the configuration of it with the username and password for our specific postrgress
    instance

--- src/tests/react-test-utils.tsx
+++ src/tests/react-test-utils.tsx
@@ -1,20 +1,20 @@
 /* eslint-disable import/export */
-import type { RenderOptions } from '@testing-library/react';
-import { render } from '@testing-library/react';
-import type { ReactElement } from 'react';
+import type { RenderOptions } from "@testing-library/react"
+import { render } from "@testing-library/react"
+import type { ReactElement } from "react"
 
 const customRender = (
   ui: ReactElement,
-  options?: Omit<RenderOptions, 'queries'>,
+  options?: Omit<RenderOptions, "queries">,
 ) =>
   render(ui, {
     wrapper: ({ children }) => <>{children}</>,
     ...options,
-  });
+  })
 
 // re-export everything
-export * from '@testing-library/react';
+export * from "@testing-library/react"
 
 // override render method
-export { customRender as render };
-export { default as userEvent } from '@testing-library/user-event';
+export { customRender as render }
+export { default as userEvent } from "@testing-library/user-event"
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
@@ -8,13 +8,13 @@ export default defineConfig({
     port: 3000,
   },
   test: {
-    environment: 'happy-dom',
+    environment: "happy-dom",
     globals: true,
-    setupFiles: ['./src/tests/setup-test-environment.ts'],
-    include: ['./src/**/*.{spec,test}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    exclude: ['node_modules', 'build', 'postgres-data'],
+    setupFiles: ["./src/tests/setup-test-environment.ts"],
+    include: ["./src/**/*.{spec,test}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["node_modules", "build", "postgres-data"],
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ["text", "json", "html"],
     },
   },
-});
+})
